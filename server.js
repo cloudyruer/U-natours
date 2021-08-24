bba568d928d775bcaa3cae6918ed6c3b8dff2504
Joey Chou <cloudyruer@gmail.com>
2021-08-24 09:55:26 +0800

    0824 mongoose

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+// 一開始載入所有環境變數
 const dotenv = require('dotenv');
 
 // NOTE order issue here since would be fixed on video 94
@@ -5,9 +7,57 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-// 4. START SERVER
+// connect will return a promise
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful! 😆'));
+
+// Mongoose uses the native JS data types
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// convention: use uppercase on model name
+const Tour = mongoose.model('Tour', tourSchema);
+
+// a new document (object) create by tour model (function constructor)/class
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR😢: ', err);
+  });
+
 //  default to localhost
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
