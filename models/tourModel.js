a6df09b1665c48ca1a66564dbea15fb85560eb23
Joey Chou <cloudyruer@gmail.com>
2021-09-05 23:28:51 +0800

    0905 import-dev-data

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,63 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a durations'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, //only work for string, will remove all the white space begin and end
+    required: [true, 'A tour must have a description'],
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+
+  images: [String], // array of string
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  startDates: [Date],
 });
 
 // convention: use uppercase on model name
